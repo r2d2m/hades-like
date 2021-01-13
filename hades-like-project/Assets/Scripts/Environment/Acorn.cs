0675a9da5bddb7e7509971ea66580f84e676f074
wille-eriksson <65874719+wille-eriksson@users.noreply.github.com>
2021-01-13 21:32:24 +0100

    Add destructible objects and example prefab

---
+++ hades-like-project/Assets/Scripts/Environment/Acorn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acorn : Destructible
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHP = 1;
+        currentHP = maxHP;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateCooldowns();
+        BreakCheck();
+    }
+}
--- hades-like-project/Assets/Scripts/Environment/Destructible.cs
+++ hades-like-project/Assets/Scripts/Environment/Destructible.cs
@@ -2,17 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Destructable : MonoBehaviour
+public class Destructible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    // Physics and logical
+    public float maxHP;
+    public float currentHP;
+    public int collisionDamage = 0;
+    protected Rigidbody2D rigidBody;
+    
+    // Estetic
+    protected SpriteRenderer spriteRenderer;
+    protected Color originalColor;
+    protected float colorTime = 0.07f;
+    protected float currentColorTime = 0.0f;
+    
+    // External
+    public GameObject floor;
+
+    void Awake() {
+        floor = GameObject.FindGameObjectWithTag("Floor");
+        spriteRenderer = gameObject.GetComponentInChildren<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
+    }
+
+    public void TakeDamage(float damage) {
+        if (damage > 0) {
+            currentHP -= damage;
+            currentColorTime = colorTime;
+            spriteRenderer.color = new Color(1.0f, 0.0f, 0.0f);
+            
+        }
+    }
+
+    protected bool BreakCheck() {
+        if (currentHP <= 0) {
+            Break();
+            return true;
+        }
+        return false;
+    }
+
+    public void Break() {
+        Destroy(gameObject);
+        float xsize = transform.localScale.x;
+        float ysize = transform.localScale.y;
+        floor.GetComponent<Grid>().UpdateArea(transform.position,xsize,ysize);
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    protected void updateCooldowns() {
+        if (currentColorTime > 0) {
+            currentColorTime -= Time.deltaTime;
+        } else {
+            currentColorTime = 0;
+            spriteRenderer.color = originalColor;
+        }
     }
 }
