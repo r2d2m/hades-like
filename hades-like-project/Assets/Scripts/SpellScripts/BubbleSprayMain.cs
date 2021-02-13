5f5a7a10003c1fd3c4d7460b26f6553fafac102c
Simoid <simonkawaji@gmail.com>
2021-02-13 04:30:55 +0100

    Fix mousevector bug for bubblespray

--- hades-like-project/Assets/Scripts/SpellScripts/BubbleSprayMain.cs
+++ hades-like-project/Assets/Scripts/SpellScripts/BubbleSprayMain.cs
@@ -22,8 +22,9 @@ public class BubbleSprayMain : Spell {
             GameObject newBubble = Instantiate(bubbleSpraySub, gunGameObject.transform.position, transform.rotation);
             newBubble.transform.parent = transform.parent;
             Spell spellScript = newBubble.GetComponent<Spell>();
-            Vector3 currMouseVec = (mouseGameObject.transform.position - gunGameObject.transform.position).normalized;
-            newBubble.GetComponent<Rigidbody2D>().AddForce(Quaternion.Euler(0, 0, Random.Range(-50, 50)) * currMouseVec * Random.Range(1000, 1700) * speedMultiplier);
+            Vector2 currMouseVec = mouseGameObject.transform.position - gunGameObject.transform.position;
+            print(currMouseVec);
+            newBubble.GetComponent<Rigidbody2D>().AddForce(Quaternion.Euler(0, 0, Random.Range(-50, 50)) * currMouseVec.normalized * Random.Range(300, 600) * speedMultiplier);
             spellScript.damage = 1f * damageMultiplier;
             spellScript.setPlayerStats(damageMultiplier, rangeMultiplier, cooldownMultiplier, speedMultiplier, lifeTimeMultiplier, forceMultipler);
             yield return new WaitForSeconds(0.1f);
