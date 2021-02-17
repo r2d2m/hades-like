af0bf45336207da340f00cec849040fbbfd1a40f
Simoid <simonkawaji@gmail.com>
2021-02-18 00:06:47 +0100

    Fix pause screen mouse bug

--- hades-like-project/Assets/Scripts/UIScripts/Menus/PauseMenu.cs
+++ hades-like-project/Assets/Scripts/UIScripts/Menus/PauseMenu.cs
@@ -3,38 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
-{
+public class PauseMenu : MonoBehaviour {
     public static bool paused = false;
     public GameObject pauseMenuUI;
 
     public MousePointerScript mouseScript;
 
-    private void Start() {
-        mouseScript.setMouseHand();
-    }
-
-
-    void Update()
-    {
+    void Update() {
         if (Input.GetKeyDown(KeyCode.Escape)) {
-           
+
             if (paused) {
                 resume();
             } else {
                 pause();
             }
         }
-        
+
     }
 
     public void resume() {
+        mouseScript.setMouseAim();
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         paused = false;
     }
 
     public void pause() {
+        mouseScript.setMouseHand();
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         paused = true;
