bfba127cde654eb174c92e5fe397512e547622c4
wille-eriksson <65874719+wille-eriksson@users.noreply.github.com>
2021-02-17 23:55:49 +0100

    Pause Menu

---
+++ hades-like-project/Assets/Scripts/UIScripts/Menus/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool paused = false;
+    public GameObject pauseMenuUI;
+
+    public MousePointerScript mouseScript;
+
+    private void Start() {
+        mouseScript.setMouseHand();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+           
+            if (paused) {
+                resume();
+            } else {
+                pause();
+            }
+        }
+        
+    }
+
+    public void resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    public void pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    public void quit() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+}
