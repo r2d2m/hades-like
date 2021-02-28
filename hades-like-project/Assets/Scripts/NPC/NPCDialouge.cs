3763f829d0adc65eea5893bd578b652e122cd36e
wille-eriksson <65874719+wille-eriksson@users.noreply.github.com>
2021-02-28 12:16:27 +0100

    Add dialogue system

---
+++ hades-like-project/Assets/Scripts/NPC/NPCDialouge.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDialouge : MonoBehaviour
+{
+    
+    public GameObject dialogueManager;
+    public GameObject dialogueCanvas;
+    public float dialogueDistance = 2;
+
+    private GameObject player;
+    private bool inDialogue = false;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+
+        if ((player.transform.position - transform.position).magnitude < dialogueDistance) {
+
+            if (inDialogue == false) {
+                inDialogue = true;
+                dialogueManager.SetActive(true);
+                dialogueCanvas.SetActive(true);
+            }
+
+        } else {
+
+            if (inDialogue) {
+                inDialogue = false;
+                dialogueManager.SetActive(false);
+                dialogueCanvas.SetActive(false);
+            }
+
+        }
+        
+    }
+}
