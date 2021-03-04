c1ede3ba165153086e14d047b213ffdf4a77b619
Simoid <simonkawaji@gmail.com>
2021-03-04 20:37:26 +0100

    Add face change according to %HP

--- hades-like-project/Assets/Scripts/UIScripts/HealthFlowerManger.cs
+++ hades-like-project/Assets/Scripts/UIScripts/HealthFlowerManger.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class HealthFlowerManger : MonoBehaviour {
     public GameObject flowerPetalContainer;
     public GameObject healthFace;
+    public Sprite[] flowerFaces;
+    public int maxHP;
+    public int currentHP;
 
     FlowerPetalManager petalManager;
     // Start is called before the first frame update
     void Awake() {
         petalManager = flowerPetalContainer.GetComponent<FlowerPetalManager>();
-        print(petalManager);
     }
 
     public void setCurrentHP(int HP) {
         petalManager.setCurrentHP(HP);
+        currentHP = HP;
+        float percentHP = (float) currentHP / maxHP;
+        int newFaceIndex = (int) ((flowerFaces.Length - 1) * percentHP);
+        print(((flowerFaces.Length - 1) * (currentHP / maxHP)));
+        print(newFaceIndex);
+        healthFace.GetComponent<SpriteRenderer>().sprite = flowerFaces[newFaceIndex];
     }
 
     public void setMaxHP(int HP) {
         petalManager.setMaxHP(HP);
+        maxHP = HP;
     }
 }
